2f40a8d1b5fef3e6e26c705c4d87b7efe7d2ae7a
Glenn Torrens <glenntorrens@gmail.com>
2025-11-19 17:07:28 +0900

    implement showcase section

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,13 @@
 import Navbar from "./sections/Navbar";
 import Hero from "./sections/Hero";
+import Showcase from "./sections/Showcase";
 
 const App = () => {
   return (
     <>
       <Navbar />
       <Hero />
+      <Showcase />
     </>
   );
 };
