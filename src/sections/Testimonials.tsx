a440c90d9d074827f1632a19ff41a897c893b7df
Glenn Torrens <glenntorrens@gmail.com>
2025-12-11 17:18:54 +0900

    implemented testimonals, need to fix images and find ts 3d logo

---
+++ src/sections/Testimonials.tsx
@@ -0,0 +1,33 @@
+import React from 'react'
+import TitleHeader from '../components/TitleHeader'
+import { testimonials } from '../constants'
+
+import GlowCard from '../components/GlowCard'
+
+const Testimonials = () => {
+  return (
+    <section id='testimonials' className='flex-center section-padding'>
+        <div className='w-full h-full md:px-10 px-5'>
+            <TitleHeader title='Word on the Street' subtitle='What My (imaginary) Clients Say About Me' />
+
+            <div className='lg:columns-3 md:columns-2 columns-1 mt-16'>
+                {testimonials.map(({imgPath, name, mentions, review}) => (
+                    <GlowCard card={{review}}>
+                      <div className='flex items-center gap-3'>
+                        <div>
+                            <img src={imgPath} alt={name} className='w-12 h-12 rounded-full object-cover' />
+                        </div>
+                        <div>
+                            <p className='font-bold'>{name}</p>
+                            <p className='text-white-50'>{mentions}</p>
+                        </div>
+                      </div>
+                    </GlowCard>
+                ))}
+            </div>
+        </div>
+    </section>
+  )
+}
+
+export default Testimonials
